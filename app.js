ad62e9a23dd62572ac8773eff9360c3394a00c12
Robert Elwood Bond III <115675969+Elwoodbond@users.noreply.github.com>
2023-04-15 17:37:57 -0400

    Update app.js
    added an additional line to code at 28 to fix the broken image link

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.post('/', function (req, res) {
             const weatherDescription = weatherData.weather[0].description;
             const icon = weatherData.weather[0].icon;
             const imageURL = "http://openweathermap.org/img/w/" + icon + "@2x.png";
+            res.header("content-type", "text/html");
             res.write("<p>The weather is currently " + weatherDescription + "</p>");
             res.write("<h1>The temperature in " + query + " is " + temp + " degrees Fahrenheit.</h1>");
             res.write("<img src=" + imageURL + ">");
